deba815806705b398bb8018003c5bdfa17fa50ab
pasternak2048 <10pka.muscatel@gmail.com>
2024-08-20 17:46:57 +0300

    Added Calculator functionality.

---
+++ YuriiPasternak.Calculator/Models/CalculatorDouble.cs
@@ -0,0 +1,32 @@
+﻿using YuriiPasternak.Calculator.Interfaces;
+
+namespace YuriiPasternak.Calculator.Models
+{
+    public class CalculatorDouble : ICalculator<double>
+    {
+        public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Min(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Mult(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Div(double x, double y)
+        {
+            if(y == 0)
+            {
+                Console.WriteLine("ERROR: YOU ARE NOT ABLE TO DIVIDE FOR ZERO.");
+            }
+
+            return x / y;
+        }
+    }
+}
